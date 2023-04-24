ce0bcf96857bac1badce9b8219f143b74df2690c
jkoh0024 <jkoh0024@student.monash.edu>
2023-04-24 19:16:37 +0800

    setup angular materials packages

--- routing-app/src/app/app.module.ts
+++ routing-app/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { TestConnectionComponent } from './test-connection/test-connection.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { TestConnectionComponent } from './test-connection/test-connection.compo
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    MatSlideToggleModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
