d3716881f15fb3708db79e067e1147d96d69dccc
jkoh0024 <jkoh0024@student.monash.edu>
2023-04-27 01:13:03 +0800

    basic dataset listing

--- routing-app/src/app/dataset/dataset.component.ts
+++ routing-app/src/app/dataset/dataset.component.ts
@@ -1,8 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, ViewChild } from '@angular/core';
 import { AgGridAngular } from 'ag-grid-angular';
 import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
+import { TestConnectionService } from '../services/test-connection-services';
+import { Store } from '@ngrx/store';
+import { TestConnectionState } from '../test-connection/store/states';
 
 @Component({
   selector: 'app-dataset',
@@ -10,11 +13,34 @@ import { Observable } from 'rxjs';
   styleUrls: ['./dataset.component.css']
 })
 export class DatasetComponent {
+  apiUrl: string;
+  httpClient: HttpClient;
+
+  constructor(httpClient: HttpClient,
+    private testConnectionServices: TestConnectionService,
+    private testConnectionStore: Store<TestConnectionState>
+    ) {
+      this.apiUrl = 'http://127.0.0.1:5000/'
+      this.httpClient = httpClient;
+  }
 
   columnDefs: ColDef[] = [
-    { field: 'make' },
-    { field: 'model' },
-    { field: 'price' }
+    { 
+      headerName: "Name",
+      field: 'name' 
+    },
+    { 
+      headerName: "Date Uploaded",
+      field: 'create_date' 
+    },
+    { 
+      headerName: "Last Updated",
+      field: 'update_date' 
+    },
+    { 
+      headerName: "MongoDB ID",
+      field: '_id' 
+    },
   ];
 
    // DefaultColDef sets props common to all Columns
@@ -29,11 +55,9 @@ export class DatasetComponent {
   // For accessing the Grid's API
   @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
 
-  constructor(private http: HttpClient) {}
-
   // Example load data from server
   onGridReady(params: GridReadyEvent) {
-    this.rowData$ = this.http
+    this.rowData$ = this.httpClient
       .get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
   }
 
@@ -46,4 +70,37 @@ export class DatasetComponent {
   clearSelection(): void {
     this.agGrid.api.deselectAll();
   }
+
+
+
+  public getHttpHeader() {
+    console.log("www")
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+      };
+      return httpOptions;
+  }
+
+
+  public getQueryTest(userId: string) {
+    // const url = this.apiUrl + ApiConfig.FACEBOOK_FEED_PATH
+    const url = this.apiUrl + '/getDataset'
+    console.log(userId)
+    console.log(url)
+    return this.httpClient.post(
+      url,
+      userId,
+      this.getHttpHeader()
+    ).subscribe(x =>{console.log(x)}
+    );
+    
+  }
+
+  refresh() {
+    this.rowData$ = this.httpClient.post<any>(this.apiUrl + '/getDataset', '{"user_id": "6435575578b04a2b1549c17b"}', this.getHttpHeader())
+    .pipe(map(response => response.message))
+  }
+
 }
