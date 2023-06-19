6240f99576e4307df033fd5111b9503f9563a1a3
jkoh0024 <jkoh0024@student.monash.edu>
2023-06-19 16:51:20 +0800

    added results listing

--- routing-app/src/app/components/results/results.component.ts
+++ routing-app/src/app/components/results/results.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef, GridApi, GridReadyEvent } from 'ag-grid-community';
 import { Chart } from 'chart.js/auto';
+import { Observable, map } from 'rxjs';
 
 import { ChartService } from 'src/app/services/chart-service';
 
@@ -10,17 +14,63 @@ import { ChartService } from 'src/app/services/chart-service';
   styleUrls: ['./results.component.css']
 })
 export class ResultsComponent {
+  apiUrl: string;
+  httpClient: HttpClient;
+  
+  private gridApi!: GridApi;
+  columnApi: any;
 
-  data = [];
+  data: any[] = [];
   linear_data: any[] = []
   svm_data: any[]  = []
   knn_data: any[]  = []
   tree_data: any[]  = []
   chart: any;
 
-  constructor(
+  constructor(httpClient: HttpClient,
     private chartService: ChartService
-  ) { }
+  ) {
+    this.apiUrl = 'http://127.0.0.1:5000/'
+    this.httpClient = httpClient;
+  }
+
+  columnDefs: ColDef[] = [
+    { 
+      checkboxSelection: true 
+    },
+    { 
+      headerName: "MongoDB Document ID",
+      field: '_id' 
+    },
+    { 
+      headerName: "ML Algorithm",
+      field: 'algo_type' 
+    },
+    { 
+      headerName: "Run Name",
+      field: 'run_name' 
+    },
+    { 
+      headerName: "Run ID",
+      field: 'run_id' 
+    },
+    { 
+      headerName: "Created Date",
+      field: 'create_date' 
+    },
+  ];
+
+  // DefaultColDef sets props common to all Columns
+  public defaultColDef: ColDef = {
+    sortable: true,
+    filter: true,
+  };
+
+  // Data that gets displayed in the grid
+  public rowData$!: Observable<any[]>;
+
+  // For accessing the Grid's API
+  @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
 
   ngOnInit() {
 
@@ -85,5 +135,31 @@ export class ResultsComponent {
     })
   }
 
+  onGridReady(params: GridReadyEvent) {
+    this.gridApi! = params.api;
+    this.columnApi = params.columnApi;
 
+    const request_body = {
+      user_id: "6435575578b04a2b1549c17b"
+    }
+    this.rowData$ = this.httpClient
+      .post<any>(this.apiUrl + '/get-results', request_body, this.getHttpHeader())
+      .pipe(map(response => response.data))
+  }
+
+  public getHttpHeader() {
+    console.log("wwwd")
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+      };
+      return httpOptions;
+  }
+
+  public displayData(){
+    const selectedNodes = this.gridApi.getSelectedNodes()
+    this.data = selectedNodes.map(node => node.data)
+    console.log(this.data)
+  }
 }
