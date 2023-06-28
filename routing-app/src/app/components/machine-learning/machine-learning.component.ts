17ad83009ad3cda34f362fd166a9c7292d6d015d
jkoh0024 <jkoh0024@student.monash.edu>
2023-06-28 20:56:32 +0800

    new nested menu for ml algo selection

--- routing-app/src/app/components/machine-learning/machine-learning.component.ts
+++ routing-app/src/app/components/machine-learning/machine-learning.component.ts
@@ -23,7 +23,8 @@ export class MachineLearningComponent {
 
   algorithmCategory: any;
   algorithms: any;
-  selectedAlgorithmId: any;
+  selectedAlgoId: any;
+  selectedAlgoName: any;
   datasetId: any;
   user_id = '6435575578b04a2b1549c17b';
   results: any;
@@ -44,16 +45,16 @@ export class MachineLearningComponent {
     this.httpClient = httpClient;
   }
 
-  regression_algorithms$ = [
-    { id: 'linear regression', name: 'Linear Regression' },
-    { id: 'decision trees', name: 'Decision Trees' },
-    { id: 'knn', name: 'K-Nearest Neighbours (KNN)' },
-    { id: 'random forests', name: 'Random Forests' },
-    { id: 'svm', name: 'Support Vector Machines (SVM)' },
-    { id: 'ensemble', name: 'Ensemble' },
-  ];
+  // regression_algorithms$ = [
+  //   { id: 'linear regression', name: 'Linear Regression' },
+  //   { id: 'decision trees', name: 'Decision Trees' },
+  //   { id: 'knn', name: 'K-Nearest Neighbours (KNN)' },
+  //   { id: 'random forests', name: 'Random Forests' },
+  //   { id: 'svm', name: 'Support Vector Machines (SVM)' },
+  //   { id: 'ensemble', name: 'Ensemble' },
+  // ];
 
-  classification_algorithms$ = [{ id: 54321, name: '(empty)' }];
+  // classification_algorithms$ = [{ id: 54321, name: '(empty)' }];
 
   dataset$ = this.datasetStore.select(selectDataset);
 
@@ -65,18 +66,18 @@ export class MachineLearningComponent {
     runName: new FormControl(''),
   });
 
-  selectCategory() {
-    if (this.algorithmCategory == 'Classification') {
-      this.algorithms = this.classification_algorithms$;
-    } else {
-      this.algorithms = this.regression_algorithms$;
-    }
-  }
+  // selectCategory() {
+  //   if (this.algorithmCategory == 'Classification') {
+  //     this.algorithms = this.classification_algorithms$;
+  //   } else {
+  //     this.algorithms = this.regression_algorithms$;
+  //   }
+  // }
 
   algorithmChange() {
     this.results = null;
 
-    if (this.selectedAlgorithmId == 'knn') {
+    if (this.selectedAlgoId == 'knn') {
       this.formFields = [
         {
           label: 'No. of Neighbours',
@@ -89,7 +90,7 @@ export class MachineLearningComponent {
       this.formData = {
         neighbours_count: 8,
       };
-    } else if (this.selectedAlgorithmId == 'decision trees') {
+    } else if (this.selectedAlgoId == 'decision trees') {
       this.formFields = [
         {
           label: 'Max Tree Depth',
@@ -115,7 +116,7 @@ export class MachineLearningComponent {
     const request_params = {
       DATASET_ID: this.datasetId,
       user_id: this.user_id,
-      algo_type: this.selectedAlgorithmId,
+      algo_type: this.selectedAlgoId,
       target_variable: this.targetVariable,
       independent_variables: selectedIndependentVariables,
       additional_params: { ...this.formData },
@@ -168,6 +169,119 @@ export class MachineLearningComponent {
   setAll(selected: boolean) {
     this.allSelected = selected;
     this.independentVariables.forEach((a) => (a.selected = selected));
+  }
+
+
+
+  onSelectAlgo(displayValue: string, algoId:string){
+    console.log(displayValue)
+    console.log(algoId)
+    this.selectedAlgoId = algoId
+    this.selectedAlgoName = displayValue
+
+    // clear display of previous results
+    this.results = null;
+
+    // update algo specific parameters expansion
+    if (this.selectedAlgoId == 'knn') {
+      this.formFields = [
+        {
+          label: 'No. of Neighbours',
+          type: 'number',
+          name: 'neighbours_count',
+          value: '',
+          min: 0,
+          required: true,
+        },
+      ];
+      this.formData = {
+        neighbours_count: 8,
+      };
+    } else if (this.selectedAlgoId == 'decision trees') {
+      this.formFields = [
+        {
+          label: 'Max Tree Depth',
+          type: 'number',
+          name: 'max_depth',
+          value: '',
+          min: 0,
+          required: true,
+        },
+      ];
+      this.formData = {
+        max_depth: 10,
+      };
+    } else if (this.selectedAlgoId == 'bagging_regression') {
+      this.formFields = [
+        {
+          label: 'No. of estimators',
+          type: 'number',
+          name: 'n_estimators',
+          value: '',
+          required: true,
+        },
+        {
+          label: 'Max samples',
+          type: 'number',
+          name: 'max_samples',
+          value: '',
+          min: 0,
+          step: 0.01,
+          required: true,
+        },
+        {
+          label: 'Max features',
+          type: 'number',
+          name: 'max_features',
+          value: '',
+          min: 0,
+          step: 0.01,
+          required: true,
+        },
+      ];
+      this.formData = {
+        n_estimators: 10,
+        max_samples: 1.0,
+        max_features: 1.0,
+      };
+    } else if (this.selectedAlgoId == 'random_forest_regression') {
+      this.formFields = [
+        {
+          label: 'No. of estimators',
+          type: 'number',
+          name: 'n_estimators',
+          value: '',
+          required: true,
+        },
+        {
+          label: 'Max depth',
+          type: 'number',
+          name: 'max_depth',
+          value: '',
+          min: 0,
+          step: 0.01,
+          required: false,
+        },
+      ];
+      this.formData = {
+        placeholder: 10,
+      };
+    } else if (this.selectedAlgoId == 'voting_regression') {
+      this.formFields = [
+        {
+          label: 'Placeholder',
+          type: 'number',
+          name: 'placeholder',
+          value: '',
+          required: false,
+        },
+      ];
+      this.formData = {
+        placeholder: 10,
+      };
+    } else {
+      this.formFields = [];
+    }
   }
 }
 
