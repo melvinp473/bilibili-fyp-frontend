65d64cfe466a67980df61fa2f6665ce5496b01f0
ylim0066 <robert.merkel@benambra.org>
2023-07-04 18:58:18 +0800

    make selection results display on bar graph

--- routing-app/src/app/components/preprocessing/feature-selection/feature-selection.component.ts
+++ routing-app/src/app/components/preprocessing/feature-selection/feature-selection.component.ts
@@ -3,6 +3,7 @@ import { PreprocssingService } from 'src/app/services/preprocessing-services';
 import { DatasetState } from '../../state-controllers/dataset-controller/states';
 import { Store } from '@ngrx/store';
 import { selectDataset } from '../../state-controllers/dataset-controller/selectors/dataset.selectors';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-feature-selection',
@@ -17,6 +18,8 @@ export class FeatureSelectionComponent {
   selectedAlgoId: any;
   selectedAlgoName: any;
   algoParamsFormData: any;
+
+  chart: any;
 
   constructor(
     private datasetStore: Store<DatasetState>,
@@ -44,13 +47,53 @@ export class FeatureSelectionComponent {
     console.log(this.algoParamsFormData)
   }
 
+  sortScore(data: any) {
+    let i, key, j; 
+    for (i = 1; i < data.length; i++)
+    { 
+        key = [data[i][0], data[i][1]]; 
+        j = i - 1; 
+
+        while (j >= 0 && data[j][1] < key[1])
+        { 
+            data[j + 1] = data[j]; 
+            j = j - 1; 
+        } 
+        data[j + 1] = key; 
+    }
+    return data
+  }
 
   runPreprocessing() {
     // console.log(typeof(this.algoParamsFormData))
-    this.preprocessingService.runPreprocessing(this.datasetId, this.selectedAlgoId, this.algoParamsFormData).subscribe(data => {
-      if(data.flag) {
-        console.log(data)
+    this.preprocessingService.runPreprocessing(this.datasetId, this.selectedAlgoId, this.algoParamsFormData).subscribe(response => {
+      if(response.flag) {
+        const data = response.body
+        this.displayData(this.sortScore(data))
+        // console.log(data)
       }
+    })
+  }
+
+  public displayData(data: any[]){
+    if (this.chart != null) {
+      this.chart.destroy()
+    }
+    this.plotGraph(data)
+  }
+
+  public plotGraph(selected_data: any[]) {
+    this.chart = new Chart('canvas', {
+      type: 'bar',
+      data: {
+        labels: selected_data.map(row => row[0]),
+        datasets:[
+          {
+            label:'ranking score',
+            data: selected_data.map(row => row[1])
+          }
+        ]
+        }
     })
   }
 
