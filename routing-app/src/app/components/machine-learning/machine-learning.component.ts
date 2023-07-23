489d0427ee5e39168709220f255af9a67382b77c
jkoh0024 <jkoh0024@student.monash.edu>
2023-07-23 18:02:29 +0800

    BF-31: put general ML info into formgroup, used angular form for required field validation

--- routing-app/src/app/components/machine-learning/machine-learning.component.ts
+++ routing-app/src/app/components/machine-learning/machine-learning.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 
 import { selectDataset, selectFeatures } from '../state-controllers/dataset-controller/selectors/dataset.selectors';
@@ -34,17 +34,16 @@ export class MachineLearningComponent implements OnInit{
   datasetId: any;
   datasetName: any;
   user_id = '6435575578b04a2b1549c17b';
-  selectedAlgoId: any;
   selectedAlgoName: any;
   results: any;
   algoParamsFormData: any;
 
-  targetVariable: any;
   independentVariables: Variable[] = [];
 
   variables: any[] = [];
   allSelected: boolean = false;
 
+  mainForm!: FormGroup;
   resultLogForm = new FormGroup({
     save_results: new FormControl(''),
     runName: new FormControl(''),
@@ -56,13 +55,22 @@ export class MachineLearningComponent implements OnInit{
     httpClient: HttpClient,
     private datasetStore: Store<DatasetState>,
     private mlWekaStore: Store,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private formBuilder: FormBuilder
   ) {
     this.apiUrl = 'http://127.0.0.1:5000/';
     this.httpClient = httpClient;
   }
 
   ngOnInit(){
+    this.mainForm = this.formBuilder.group({
+      dataset_id: ['', Validators.required],
+      user_id: [this.user_id, Validators.required],
+      algo_type: ['', Validators.required],
+      target_variable: ['', Validators.required],
+      independent_variables: ['',],
+    });
+
     this.subs.sink = this.mlWekaStore$.subscribe((results) => {
       console.log(results);
       
@@ -80,13 +88,14 @@ export class MachineLearningComponent implements OnInit{
     this.subs.sink = this.dataset$.subscribe((data) => {
       console.log(data)
       this.datasetId = data._id;
+      this.mainForm.patchValue({dataset_id: data._id})
       this.datasetName = data.name;
       this.variables = data.attributes;
     });
 
     this.subs.sink = this.features$.subscribe((results) => {
       if (results != null && results.length != 0) {
-        this.targetVariable = results[0]
+        this.mainForm.patchValue({target_variable: results[0]})
         // console.log(results)
         // console.log(this.variables)
         const set2 = new Set(results)
@@ -120,10 +129,10 @@ export class MachineLearningComponent implements OnInit{
       .map((variable) => variable.name);
 
     const request_params = {
-      DATASET_ID: this.datasetId,
-      user_id: this.user_id,
-      algo_type: this.selectedAlgoId,
-      target_variable: this.targetVariable,
+      DATASET_ID: this.mainForm.controls['dataset_id'].value,
+      user_id: this.mainForm.controls['user_id'].value,
+      algo_type: this.mainForm.controls['algo_type'].value,
+      target_variable: this.mainForm.controls['target_variable'].value,
       independent_variables: selectedIndependentVariables,
       algo_params: {...this.algoParamsFormData},
       result_logging: { ...this.resultLogForm.value },
@@ -139,7 +148,7 @@ export class MachineLearningComponent implements OnInit{
   }
 
   onSelectAlgo(displayValue: string, algoId:string){
-    this.selectedAlgoId = algoId
+    this.mainForm.patchValue({algo_type: algoId})
     this.selectedAlgoName = displayValue
 
     this.algoParamsFormData = null;
@@ -150,7 +159,7 @@ export class MachineLearningComponent implements OnInit{
   onSelectTarget() {
     this.independentVariables = []
     this.variables
-      .filter((a) => a != this.targetVariable)
+      .filter((a) => a != this.mainForm.get("target_variable")?.value)
       .forEach((variable: any) => {
         this.independentVariables.push({
           name: variable,
